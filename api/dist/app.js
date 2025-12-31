188e84b4ebb9b55282085dc198ddcd93f8c78859
dttHuongg <dev.hoangthanhnam.1008@gmail.com>
2025-12-31 11:33:24 +0700

    chore(build): add package for ts

--- api/dist/app.js
+++ api/dist/app.js
@@ -13,7 +13,7 @@ const auth_1 = require("./middleware/auth");
 const order_route_1 = __importDefault(require("./routes/order.route"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({ origin: "http://localhost:3000", credentials: true }));
+app.use((0, cors_1.default)({ origin: ["http://localhost:3000", "https://ecommerce-platform-web-drab.vercel.app"], credentials: true }));
 app.use("/api/webhook", webhook_route_1.default);
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
